60e22d944c06ad679e28efff32d08874511279eb
thomasguillard <thomas.guillardg@gmail.com>
2017-02-06 19:38:17 +0000

    Folder Structure

---
+++ XYZ.tingapp/main.py
@@ -0,0 +1,12 @@
+import tingbot
+from tingbot import *
+
+# setup code here
+
+@every(seconds=1.0/30)
+def loop():
+    # drawing code here
+    screen.fill(color='black')
+    screen.text('X-Y-Z')
+
+tingbot.run()
